114096d6cbc1cf45e8ada7072aa79fd7422840a7
Nget-Darapich <ngetdarapich23@gmail.com>
2026-03-04 09:49:14 +0700

    TP01

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
 import { TaskModule } from './modules/task/task.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../src/modules/user/user.entity';
+import { Task } from '../src/modules/task/task.entity';
 
 @Module({
-  imports: [UserModule, TaskModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'todo.sqlite',
+      entities: [User, Task], // entities is a representation of the tables in the database, we need to add all the entities that we want to use in our application!
+      synchronize: true, // use only in development!
+    }),
+    UserModule,
+    TaskModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
